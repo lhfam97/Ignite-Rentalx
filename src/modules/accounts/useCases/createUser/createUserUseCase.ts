7a9e0e1cc1f9c0f34cdc257a98ccbd09be3d0cfc
Luís Henrique Machado <lhfam97@gmail.com>
2021-05-20 07:08:33 -0300

    Entendendo autenticacao com JWT

--- src/modules/accounts/useCases/createUser/createUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/createUserUseCase.ts
@@ -1,6 +1,7 @@
 import { inject, injectable } from "tsyringe";
+import { hash } from "bcrypt";
+
 import { ICreateUserDTO } from "../../dtos/ICreateUsrDTO";
-import { UsersRepository } from "../../repositories/implementantions/UsersRepository";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 @injectable()
@@ -12,16 +13,22 @@ class CreateUserUseCase {
 
   async execute({
     name,
-    username,
     email,
     password,
     driver_license,
   }: ICreateUserDTO): Promise<void> {
+    const userAlreadyExists = await this.usersRepository.findByEmail(email);
+
+    if (userAlreadyExists) {
+      throw new Error("User already exists");
+    }
+
+    const passwordHash = await hash(password, 8);
+
     await this.usersRepository.create({
       name,
-      username,
       email,
-      password,
+      password: passwordHash,
       driver_license,
     });
   }
