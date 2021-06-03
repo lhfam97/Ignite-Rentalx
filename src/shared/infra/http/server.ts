d63d66262bccdf4350e1521f69c3d13541a3e1b9
Luís Henrique Machado <lhfam97@gmail.com>
2021-06-03 17:16:17 -0300

    Testes de integracao nao funcionando

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,36 +1,3 @@
-import express, { Request, Response, NextFunction } from "express";
-import "express-async-errors";
-import swaggerUI from "swagger-ui-express";
-
-import createConnection from "@shared/infra/typeorm";
-import "@shared/container";
-
-import { router } from "@shared/infra/http/routes";
-import swaggerFile from "../../../swagger.json";
-import { AppError } from "@shared/errors/AppError";
-
-// import cors from "cors";
-createConnection("localhost");
-const app = express();
-app.use(express.json());
-app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
-app.use(router);
-
-app.use(
-  (err: Error, request: Request, response: Response, next: NextFunction) => {
-    if (err instanceof AppError) {
-      return response.status(err.statusCode).json({
-        message: err.message,
-      });
-    } else {
-      return response.status(500).json({
-        status: "error",
-        message: `Internal Server Error - ${err.message}`,
-      });
-    }
-  }
-);
-
-// app.use(cors);
+import { app } from "./app";
 
 app.listen(3333, () => console.log("Server is running!!"));
